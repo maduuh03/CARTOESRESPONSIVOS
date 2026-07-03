ed111de12c81093f234ff41ab7eaed383840e67a
maduuh03 <maria.silva0303@escola.pr.gov.br>
2026-07-03 12:47:26 +0000

    oooooooooooooooooooooooooooooooooooooooooooooooooooo

---
+++ script.js
@@ -0,0 +1,27 @@
+const botao1 = document.getElementById("botao1");
+const botao2 = document.getElementById("botao2");
+const botao3 = document.getElementById("botao3");
+const botao4 = document.getElementById("botao4");
+const botao5 = document.getElementById("botao5");
+
+const caixa = document.getElementById("caixa-cartoes");
+
+botao1.addEventListener('click', () => {
+    alert("aaaaaaaaaaaaaaaa");
+})
+
+botao2.addEventListener('click', () => {
+    alert("aaaaaaaaaaaaaaaa");
+})
+
+botao3.addEventListener('click', () => {
+    alert("aaaaaaaaaaaaaaaa");
+})
+
+botao4.addEventListener('click', () => {
+    alert("aaaaaaaaaaaaaaaa");
+})
+
+botao5.addEventListener('click', () => {
+    alert("aaaaaaaaaaaaaaaa");
+})
